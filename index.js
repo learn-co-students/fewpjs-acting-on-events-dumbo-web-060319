8cfc300f97b2a686b7ea0fdc95f222f27403ba42
Alexander Schelchere <ascellie@gmail.com>
2019-07-15 15:12:55 -0400

    Thank you Franchely!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+let dodger = document.querySelector('#dodger');
+
+dodger.style.backgroundColor = '#FF69B4';
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace('px', '');
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  console;
+  if (e.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  }
+  if (e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
